9ccfb3c88de2407428fb9689d3a18a46f6d12155
Jose Her <jlherrera1000@gmail.com>
2015-03-07 16:08:35 -0600

    Minor changes

--- app/src/main/java/me/jlhp/sivale/MainActivity.java
+++ app/src/main/java/me/jlhp/sivale/MainActivity.java
@@ -2,49 +2,52 @@ package me.jlhp.sivale;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.alexgilleran.icesoap.exception.SOAPException;
-import com.alexgilleran.icesoap.exception.XMLParsingException;
-import com.alexgilleran.icesoap.observer.SOAP11Observer;
-import com.alexgilleran.icesoap.parser.impl.IceSoapParserImpl;
-import com.alexgilleran.icesoap.request.Request;
-import com.alexgilleran.icesoap.request.RequestFactory;
-import com.alexgilleran.icesoap.request.SOAP11Request;
-import com.alexgilleran.icesoap.request.impl.RequestFactoryImpl;
-import com.alexgilleran.icesoap.soapfault.SOAP11Fault;
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 
-import org.apache.http.Header;
-import org.apache.http.entity.StringEntity;
-
-import java.io.ByteArrayInputStream;
-import java.io.UnsupportedEncodingException;
+import de.greenrobot.event.EventBus;
+import me.jlhp.sivale.api.SiValeClientAPI;
+import me.jlhp.sivale.event.ErrorEvent;
+import me.jlhp.sivale.event.FaultEvent;
+import me.jlhp.sivale.event.GetBalanceEvent;
+import me.jlhp.sivale.event.GetTransactionsEvent;
+import me.jlhp.sivale.event.LoginEvent;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+    private SiValeClientAPI client = new SiValeClientAPI();
+
+    Button b;
+    TextView t1;
+    TextView t2;
+    EditText e1;
+    EditText e2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            SiValeClientAPI client = new SiValeClientAPI();
-            client.login(this, getString(R.string.card_number), getString(R.string.passwd));
-        }
-    }
+        b = (Button) findViewById(R.id.button);
+        t1 = (TextView) findViewById(R.id.textView);
+        t2 = (TextView) findViewById(R.id.textView2);
+        e1 = (EditText) findViewById(R.id.editText);
+        e2 = (EditText) findViewById(R.id.editText2);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                client.login(MainActivity.this, e2.getText().toString(), e1.getText().toString());
+            }
+        });
     }
 
     @Override
@@ -60,5 +63,92 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        registerEventBus();
+    }
+
+    @Override
+    public void onStop() {
+        unregisterEventBus();
+        super.onStop();
+    }
+
+    public void onEvent(GetBalanceEvent getBalanceEvent) {
+        t1.setText("$" + getBalanceEvent.getBalanceData().getBalance());
+        t2.setText(getBalanceEvent.getBalanceData().getSessionData().getCardNumber());
+
+        showToast(getBalanceEvent.getBalanceData().getBalance() + "");
+        unregisterStickyEvent(getBalanceEvent);
+    }
+
+    public void onEvent(GetTransactionsEvent getTransactionsEvent) {
+
+    }
+
+    public void onEvent(LoginEvent loginEvent) {
+        if (loginEvent.hasRetryOperation()) {
+            switch (loginEvent.getRetryOperation()) {
+                case GET_BALANCE:
+                    client.getBalance(this, loginEvent.getSessionData().getSessionId());
+                    break;
+                case GET_TRANSACTIONS:
+                    client.getTransactions(this, loginEvent.getSessionData().getSessionId());
+                    break;
+            }
+        } else {
+            client.getBalance(this, loginEvent.getSessionData().getSessionId());
+        }
+
+        unregisterStickyEvent(loginEvent);
+    }
+
+    public void onEvent(FaultEvent faultEvent) {
+        switch (faultEvent.getOperation()) {
+            case GET_BALANCE:
+            case GET_TRANSACTIONS:
+            case LOGIN:
+                showToast("Error de conexión. Favor de intentar más tarde");
+                break;
+        }
+
+        unregisterStickyEvent(faultEvent);
+    }
+
+    public void onEvent(ErrorEvent errorEvent) {
+        if ("NO EXISTE SESION".equalsIgnoreCase(errorEvent.getError())) {
+            //TODO: do below stuff
+            //if(rememberPassword()){
+            //    client.login(this, storedCardNumber, storedCardPassword, SiValeOperation.GET_BALANCE);
+            //}
+            //else {
+            //    promptForCredentials();
+            //}
+            showToast("NO SESSION ERROR. Not implemented");
+        } else {
+            //TODO: Log this don't show it;
+            showToast(errorEvent.getError());
+        }
+
+        unregisterStickyEvent(errorEvent);
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+    }
+
+    private void registerEventBus() {
+        EventBus.getDefault().registerSticky(this);
+    }
+
+    private void unregisterEventBus() {
+        EventBus.getDefault().unregister(this);
+    }
+
+    private void unregisterStickyEvent(Object o) {
+        EventBus.getDefault().removeStickyEvent(o);
     }
 }
